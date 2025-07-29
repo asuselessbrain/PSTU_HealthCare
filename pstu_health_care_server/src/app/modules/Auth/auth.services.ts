fb38ecd5481c84ff51b16ac8e732482c40d4dae0
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-07-29 21:30:31 +0600

    add: generate access troken using jwt

--- pstu_health_care_server/src/app/modules/Auth/auth.services.ts
+++ pstu_health_care_server/src/app/modules/Auth/auth.services.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../../shared/prisma";
-import bcrypt from 'bcrypt';;
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const logIn = async(payload: {email: string, password: string}) => {
     const isUserExist = await prisma.user.findUniqueOrThrow({
@@ -14,7 +15,8 @@ const logIn = async(payload: {email: string, password: string}) => {
         throw new Error("Email or password does not matched!")
     }
 
-    
+    const accessToken = jwt.sign({email: isUserExist.email, role: isUserExist.role}, "abcdefg", {algorithm: "HS256", expiresIn: "15m"})
+    console.log(accessToken)
 }
 
 export const authServices = {
