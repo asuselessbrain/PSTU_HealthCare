900138ec5b0ae9f55941600b5913c0dd17111886
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-07-29 19:39:58 +0600

    add: password hashed before store in db

--- pstu_health_care_server/src/app/modules/Users/user.service.ts
+++ pstu_health_care_server/src/app/modules/Users/user.service.ts
@@ -1,12 +1,15 @@
 import { PrismaClient, UserRole } from "../../../../generated/prisma"
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 const createUserInDB = async(data: any) => {
 
+    const hashedPassword = await bcrypt.hash(data.password, 12)
+
     const userData = {
         email: data.admin.email,
-        password: data.password,
+        password: hashedPassword,
         role: UserRole.ADMIN
     }
     const result = await prisma.$transaction(async(transaction)=> {
