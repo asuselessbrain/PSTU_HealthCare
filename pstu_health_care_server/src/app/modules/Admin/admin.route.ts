fb379b998e6fca4a694d056769139a9e17a7a540
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-07-29 14:32:33 +0600

    add: update admin route

--- pstu_health_care_server/src/app/modules/Admin/admin.route.ts
+++ pstu_health_care_server/src/app/modules/Admin/admin.route.ts
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get('/', adminControllers.getAllAdmin)
 router.get('/:id', adminControllers.getSingleAdmin)
+router.patch('/:id', adminControllers.updateAdminInDB)
 
 export const adminRoutes = router
