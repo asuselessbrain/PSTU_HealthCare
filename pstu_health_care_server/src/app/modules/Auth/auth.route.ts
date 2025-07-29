218d5d5dbd991fa3777e1ecebe420418fdddbb2a
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-07-29 21:01:26 +0600

    add: login validation

--- pstu_health_care_server/src/app/modules/Auth/auth.route.ts
+++ pstu_health_care_server/src/app/modules/Auth/auth.route.ts
@@ -1,3 +1,9 @@
 import express from 'express'
+import { authController } from './auth.controller';
+import { validateRequest } from '../../middleWares/validateRequest';
+import { loginValidation } from './auth.validation';
 
 const router = express.Router()
+
+router.post('/login', validateRequest(loginValidation), authController.logIn)
+export const authRoutes = router;
---
+++ pstu_health_care_server/src/app/modules/Auth/auth.validation.ts
@@ -0,0 +1,6 @@
+import z from "zod";
+
+export const loginValidation = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string()
+})
