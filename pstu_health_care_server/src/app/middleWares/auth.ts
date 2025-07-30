052ed260bfb7139d8a02c2b5f56b4b8e21e0da4e
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-07-30 15:17:14 +0600

    add: reusable auth middle ware

---
+++ pstu_health_care_server/src/app/middleWares/auth.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import AppError from "../errors/AppError";
+import status from "http-status";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import { config } from "../../config";
+import { prisma } from "../../shared/prisma";
+import { UserStatus } from "../../../generated/prisma";
+
+const auth =(...roles: string[]) => {
+    return async(req: Request, res: Response, next: NextFunction)=>{
+        const token = req.headers.authorization;
+
+        if(!token){
+            throw new AppError(status.UNAUTHORIZED, "You are not authorized")
+        }
+
+        let decoded;
+
+        try{
+            decoded = jwt.verify(token, config.jwt.access_token_secret as Secret) as JwtPayload
+        } catch(err){
+            next(err)
+        }
+
+        const userData = await prisma.user.findUniqueOrThrow({
+            where: {
+                email: decoded?.email,
+                status: UserStatus.ACTIVE
+            }
+        })
+
+        if(roles.length && !roles.includes(decoded?.role)){
+            throw new AppError(status.UNAUTHORIZED, "Unauthorized access")
+        }
+        req.user = decoded
+        next()
+    }
+}
+
+export default auth;
--- pstu_health_care_server/src/app/modules/Users/user.route.ts
+++ pstu_health_care_server/src/app/modules/Users/user.route.ts
@@ -2,45 +2,11 @@ import  express, { NextFunction, Request, Response } from 'express';
 import { userControllers } from './user.controller';
 import { validateRequest } from '../../middleWares/validateRequest';
 import { adminValidation } from './user.validation';
-import AppError from '../../errors/AppError';
-import status from 'http-status';
-import jwt, { JwtPayload, Secret } from "jsonwebtoken"
-import { config } from '../../../config';
-import { UserRole, UserStatus } from '../../../../generated/prisma';
-import { prisma } from '../../../shared/prisma';
+import { UserRole } from '../../../../generated/prisma';
+import auth from '../../middleWares/auth';
 
 const router = express.Router()
 
-const auth =(...roles: string[]) => {
-    return async(req: Request, res: Response, next: NextFunction)=>{
-        const token = req.headers.authorization;
-
-        if(!token){
-            throw new AppError(status.UNAUTHORIZED, "You are not authorized")
-        }
-
-        let decoded;
-
-        try{
-            decoded = jwt.verify(token, config.jwt.access_token_secret as Secret) as JwtPayload
-        } catch(err){
-            next(err)
-        }
-
-        const userData = await prisma.user.findUniqueOrThrow({
-            where: {
-                email: decoded?.email,
-                status: UserStatus.ACTIVE
-            }
-        })
-
-        if(roles.length && !roles.includes(decoded?.role)){
-            throw new AppError(status.UNAUTHORIZED, "Unauthorized access")
-        }
-        next()
-    }
-}
-
-router.post("/", auth(UserRole.ADMIN), validateRequest(adminValidation), userControllers.createUser)
+router.post("/", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), validateRequest(adminValidation), userControllers.createUser)
 
 export const userRoutes = router
