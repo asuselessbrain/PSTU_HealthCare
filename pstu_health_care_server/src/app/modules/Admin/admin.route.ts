87f13ff5c0f8e9bf19d44e4147440ab369fe2c4c
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-07-30 15:30:17 +0600

    Add: change password router

--- pstu_health_care_server/src/app/modules/Admin/admin.route.ts
+++ pstu_health_care_server/src/app/modules/Admin/admin.route.ts
@@ -2,17 +2,19 @@ import express, { NextFunction, Request, Response } from 'express';
 import { adminControllers } from './admin.controller';
 import { validateRequest } from '../../middleWares/validateRequest';
 import { validateUpdateAdmin } from './admin.validation';
+import auth from '../../middleWares/auth';
+import { UserRole } from '../../../../generated/prisma';
 const router = express.Router();
 
 // ! Get All Admin From DB route
-router.get('/', adminControllers.getAllAdmin)
+router.get('/', auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminControllers.getAllAdmin)
 // ! Get Single Admin From DB using id route
-router.get('/:id', adminControllers.getSingleAdmin)
+router.get('/:id', auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminControllers.getSingleAdmin)
 // ! Update Admin Into DB route
-router.patch('/:id', validateRequest(validateUpdateAdmin), adminControllers.updateAdminInDB)
+router.patch('/:id', auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), validateRequest(validateUpdateAdmin), adminControllers.updateAdminInDB)
 // ! Hard Delete Admin From DB route
-router.delete('/:id', adminControllers.hardDeleteAdmin)
+router.delete('/:id', auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminControllers.hardDeleteAdmin)
 // ! Soft Delete Admin From DB route
-router.patch('/soft/:id', adminControllers.softDeleteAdmin)
+router.patch('/soft/:id', auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminControllers.softDeleteAdmin)
 
 export const adminRoutes = router
--- pstu_health_care_server/src/app/modules/Auth/auth.route.ts
+++ pstu_health_care_server/src/app/modules/Auth/auth.route.ts
@@ -2,11 +2,14 @@ import express, { NextFunction, Request, Response } from 'express'
 import { authController } from './auth.controller';
 import { validateRequest } from '../../middleWares/validateRequest';
 import { loginValidation } from './auth.validation';
+import auth from '../../middleWares/auth';
+import { UserRole } from '../../../../generated/prisma';
 
 const router = express.Router()
 
 
 
 router.post('/login', validateRequest(loginValidation), authController.logIn);
-router.post('/generate-token', authController.generateTokenUsingRefreshToken)
+router.post('/generate-token', authController.generateTokenUsingRefreshToken);
+router.post('/change-password', auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT), authController.changePassword);
 export const authRoutes = router;
