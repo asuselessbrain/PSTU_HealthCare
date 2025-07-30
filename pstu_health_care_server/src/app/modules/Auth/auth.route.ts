09a9bd048f4f5a48fc759f805250bf7cdeb18a96
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-07-30 15:10:11 +0600

    add: auth gurd middle ware

--- pstu_health_care_server/src/app/modules/Auth/auth.route.ts
+++ pstu_health_care_server/src/app/modules/Auth/auth.route.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { authController } from './auth.controller';
 import { validateRequest } from '../../middleWares/validateRequest';
 import { loginValidation } from './auth.validation';
 
 const router = express.Router()
+
+
 
 router.post('/login', validateRequest(loginValidation), authController.logIn);
 router.post('/generate-token', authController.generateTokenUsingRefreshToken)
--- pstu_health_care_server/src/app/modules/Users/user.route.ts
+++ pstu_health_care_server/src/app/modules/Users/user.route.ts
@@ -1,9 +1,46 @@
-import  express, { Request, Response } from 'express';
+import  express, { NextFunction, Request, Response } from 'express';
 import { userControllers } from './user.controller';
 import { validateRequest } from '../../middleWares/validateRequest';
 import { adminValidation } from './user.validation';
+import AppError from '../../errors/AppError';
+import status from 'http-status';
+import jwt, { JwtPayload, Secret } from "jsonwebtoken"
+import { config } from '../../../config';
+import { UserRole, UserStatus } from '../../../../generated/prisma';
+import { prisma } from '../../../shared/prisma';
+
 const router = express.Router()
 
-router.post("/", validateRequest(adminValidation), userControllers.createUser)
+const auth =(...roles: string[]) => {
+    return async(req: Request, res: Response, next: NextFunction)=>{
+        const token = req.headers.authorization;
+
+        if(!token){
+            throw new AppError(status.UNAUTHORIZED, "You are not authorized")
+        }
+
+        let decoded;
+
+        try{
+            decoded = jwt.verify(token, config.jwt.access_token_secret as Secret) as JwtPayload
+        } catch(err){
+            next(err)
+        }
+
+        const userData = await prisma.user.findUniqueOrThrow({
+            where: {
+                email: decoded?.email,
+                status: UserStatus.ACTIVE
+            }
+        })
+
+        if(roles.length && !roles.includes(decoded?.role)){
+            throw new AppError(status.UNAUTHORIZED, "Unauthorized access")
+        }
+        next()
+    }
+}
+
+router.post("/", auth(UserRole.ADMIN), validateRequest(adminValidation), userControllers.createUser)
 
 export const userRoutes = router
