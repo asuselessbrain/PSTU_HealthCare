545dfc57fdb4d85f41e5bfbaecdedb1d52081d86
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-07-31 08:34:07 +0600

    add: reset password controller

--- pstu_health_care_server/src/app/modules/Auth/auth.controller.ts
+++ pstu_health_care_server/src/app/modules/Auth/auth.controller.ts
@@ -4,10 +4,10 @@ import { authServices } from "./auth.services";
 import sendResponse from "../../../shared/sendResponse";
 import status from "http-status";
 
-const logIn = catchAsync(async(req: Request, res: Response)=>{
+const logIn = catchAsync(async (req: Request, res: Response) => {
     const result = await authServices.logIn(req.body);
-    const {refreshToken} = result;
-    res.cookie('refreshToken', refreshToken, {secure: false, httpOnly: true})
+    const { refreshToken } = result;
+    res.cookie('refreshToken', refreshToken, { secure: false, httpOnly: true })
     sendResponse(res, {
         statusCode: status.OK,
         message: "User logged in successfully!",
@@ -18,7 +18,7 @@ const logIn = catchAsync(async(req: Request, res: Response)=>{
     })
 })
 
-const generateTokenUsingRefreshToken = catchAsync(async(req: Request, res: Response) => {
+const generateTokenUsingRefreshToken = catchAsync(async (req: Request, res: Response) => {
     const refreshToken = req.cookies.refreshToken
     const result = await authServices.generateTokenUsingRefreshToken(refreshToken)
     sendResponse(res, {
@@ -28,8 +28,8 @@ const generateTokenUsingRefreshToken = catchAsync(async(req: Request, res: Respo
     })
 })
 
-const changePassword = catchAsync(async(req :Request&{user?: any}, res: Response) => {
-    const {email} = req.user;
+const changePassword = catchAsync(async (req: Request & { user?: any }, res: Response) => {
+    const { email } = req.user;
     const result = await authServices.changePassword(email, req?.body)
     sendResponse(res, {
         statusCode: status.OK,
@@ -38,18 +38,30 @@ const changePassword = catchAsync(async(req :Request&{user?: any}, res: Response
     })
 })
 
-const forgetPassword = catchAsync(async(req :Request, res: Response) =>{
-    const result = await authServices.forgetPassword(req.body);
+const forgetPassword = catchAsync(async (req: Request, res: Response) => {
+    await authServices.forgetPassword(req.body);
     sendResponse(res, {
         statusCode: status.OK,
         message: "Reset password link send to your email!",
-        data: result
+        data: null
     })
+})
+
+const resetPassword = catchAsync(async (req: Request & { user?: any }, res: Response) => {
+    const token = req.user;
+    await authServices.resetPassword(token, req.body)
+
+    sendResponse(res, {
+        statusCode: status.OK,
+        message: "Password has been reset successfully.",
+        data: null
+    });
 })
 
 export const authControllers = {
     logIn,
     generateTokenUsingRefreshToken,
     changePassword,
-    forgetPassword
+    forgetPassword,
+    resetPassword
 }
