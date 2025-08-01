319e1b03005d5682fcfa84b113800122ada4dfab
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-08-01 20:25:08 +0600

    add: update specialties service

--- pstu_health_care_server/src/app/modules/Specialties/Specialties.service.ts
+++ pstu_health_care_server/src/app/modules/Specialties/Specialties.service.ts
@@ -4,18 +4,18 @@ import { prisma } from "../../../shared/prisma";
 import AppError from "../../errors/AppError";
 import { uploadToCloudinary } from "../../../shared/imageUploader";
 
-const createSpecialtiesInDB = async(file: IFile,payload: {title: string, icon: string}) => {
+const createSpecialtiesInDB = async (file: IFile, payload: { title: string, icon: string }) => {
     const isSpecialtiesExist = await prisma.specialties.findUnique({
         where: {
             title: payload.title
         }
     })
 
-    if(isSpecialtiesExist){
+    if (isSpecialtiesExist) {
         throw new AppError(status.BAD_REQUEST, "Specialties already exist!")
     }
 
-    if(file){
+    if (file) {
         const icon = await uploadToCloudinary(file)
         payload.icon = icon?.secure_url as string
     }
@@ -25,6 +25,26 @@ const createSpecialtiesInDB = async(file: IFile,payload: {title: string, icon: s
     })
 
     return result
+}
+
+const updateSpecialtiesInDB = async (file: IFile, id: string, payload: { title: string, icon: string }) => {
+    const specialtiesInfo = await prisma.specialties.findUniqueOrThrow({
+        where: {
+            id
+        }
+    })
+
+    if (file) {
+        const icon = await uploadToCloudinary(file)
+        payload.icon = icon?.secure_url as string
+    }
+
+    const updatedInfo = await prisma.specialties.update({
+        where: {
+            id: specialtiesInfo.id
+        },
+        data: payload
+    })
 }
 
 export const specialtiesServices = {
