fb04e9aaf76d35d0d2d3fb9e956931587785d2fb
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-08-01 20:03:37 +0600

    add: create specialties services

---
+++ pstu_health_care_server/src/app/modules/Specialties/Specialties.service.ts
@@ -0,0 +1,28 @@
+import status from "http-status";
+import { IFile } from "../../../interfaces/file";
+import { prisma } from "../../../shared/prisma";
+import AppError from "../../errors/AppError";
+import { uploadToCloudinary } from "../../../shared/imageUploader";
+
+const createSpecialtiesInDB = async(file: IFile,payload: {title: string, icon: string}) => {
+    const isSpecialtiesExist = await prisma.specialties.findUnique({
+        where: {
+            title: payload.title
+        }
+    })
+
+    if(isSpecialtiesExist){
+        throw new AppError(status.BAD_REQUEST, "Specialties already exist!")
+    }
+
+    if(file){
+        const icon = await uploadToCloudinary(file)
+        payload.icon = icon?.secure_url as string
+    }
+
+    const result = await prisma.specialties.create({
+        data: payload
+    })
+
+    return result
+}
