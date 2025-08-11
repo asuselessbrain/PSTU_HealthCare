deaf88f5dd756ab19d4ee7c689a23f81abb1a4fc
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-08-11 23:07:25 +0600

    add: pagination in gatting all doctor

--- pstu_health_care_server/src/app/modules/Doctor/doctor.controller.ts
+++ pstu_health_care_server/src/app/modules/Doctor/doctor.controller.ts
@@ -3,9 +3,12 @@ import { catchAsync } from "../../../shared/catchAsync";
 import { doctorServices } from "./doctor.services";
 import sendResponse from "../../../shared/sendResponse";
 import status from "http-status";
+import pick from "../../../shared/pickFunction";
+import { paginationAndSortingFields } from "../Admin/admin.constant";
 
 const getAllDoctor = catchAsync(async(req: Request, res: Response) => {
     const query = req.query;
+
     const result = await doctorServices.getAllDoctorFromDB(query)
 
     sendResponse(res, {
--- pstu_health_care_server/src/app/modules/Doctor/doctor.services.ts
+++ pstu_health_care_server/src/app/modules/Doctor/doctor.services.ts
@@ -1,12 +1,23 @@
 
 import { Prisma } from "../../../../generated/prisma";
+import pagination from "../../../healper/paginationHealper";
 import { filtering } from "../../../shared/filtering";
 import { prisma } from "../../../shared/prisma"
 import { searching } from "../../../shared/searching";
 import { doctorSearchFields } from "./doctor.constant";
 
 const getAllDoctorFromDB = async (query: any) => {
-    const { searchTerm, ...filterData } = query;
+    const { searchTerm, page, limit, sortBy, sortOrder, ...filterData } = query;
+
+    const options = {
+        page: Number(page) || 1,
+        limit: Number(limit) || 10,
+        sortBy: String(sortBy || "createdAt"),
+        sortOrder: String(sortOrder || "asc")
+    };
+
+    const { page: currentPage, skip, take } = pagination(options);
+
     let searchItems: Prisma.DoctorWhereInput[] = []
     if (searchTerm) {
         searching(searchTerm, searchItems, doctorSearchFields)
@@ -16,14 +27,36 @@ const getAllDoctorFromDB = async (query: any) => {
         filtering(searchItems, filterData)
 
     }
+
     const orCondition: Prisma.DoctorWhereInput = { AND: searchItems }
 
 
     const result = await prisma.doctor.findMany({
+        where: orCondition,
+        skip,
+        take,
+        orderBy: {
+            [sortBy]: sortOrder
+        },
+        include: {
+            doctorSpecialties: true
+        }
+    })
+    const total = await prisma.doctor.count({
         where: orCondition
     })
-    return result
+
+    return {
+        meta: {
+            page: currentPage,
+            limit: take,
+            total
+        },
+        result
+    }
 }
+
+
 
 export const doctorServices = {
     getAllDoctorFromDB
