4645ad437698eaeb8dfa73d3e20d9e22323aa654
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-08-11 23:16:27 +0600

    add: get single doctor router

--- pstu_health_care_server/src/app/modules/Doctor/doctor.route.ts
+++ pstu_health_care_server/src/app/modules/Doctor/doctor.route.ts
@@ -4,5 +4,6 @@ import { doctorControllers } from './doctor.controller';
 const router = express.Router();
 
 router.get('/', doctorControllers.getAllDoctor)
+router.get('/:id', doctorControllers.getSingleDoctor)
 
 export const doctorRoutes = router
