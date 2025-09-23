39d862533b1045811d6ac2f8b94b84382258cf2d
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-09-23 21:44:39 +0600

    update: improve efficiency of create schedule functionality

--- pstu_health_care_server/src/app/modules/Schedule/schedule.service.ts
+++ pstu_health_care_server/src/app/modules/Schedule/schedule.service.ts
@@ -1,33 +1,54 @@
 import { addHours, addMinutes, format, setMinutes } from "date-fns";
+import { prisma } from "../../../shared/prisma";
 
 const createScheduleInDB = async (payload: any) => {
-    const { startDate, endDate, startTime, endTime } = payload
+    const { startDate, endDate, startTime, endTime } = payload;
+
     const currentDate = new Date(startDate)
     const lastDate = new Date(endDate)
+
+    const schedule = []
 
     while (currentDate <= lastDate) {
         const startDateTime = new Date(
             addMinutes(
-                addHours(startDate, Number(startTime.split(":")[0])),
+                addHours(`${format(currentDate, "yyyy-MM-dd")}`, Number(startTime.split(":")[0])),
                 Number(startTime.split(":")[1])
             )
         )
 
         const endDateTime = new Date(
-            setMinutes(
-                addHours(currentDate, Number(endTime.split(":")[0])),
+            addMinutes(
+                addHours(`${format(currentDate, "yyyy-MM-dd")}`, Number(endTime.split(":")[0])),
                 Number(endTime.split(":")[1])
             )
         )
+
         while (startDateTime < endDateTime) {
-            const intervalTime = {
-                startDateTime: startDateTime,
+            const interval = {
+                startDateTime,
                 endDateTime: addMinutes(startDateTime, 30)
             }
+
+            const isScheduleExist = await prisma.schedule.findFirst({
+                where: {
+                    startDateTime: interval.startDateTime,
+                    endDateTime: interval.endDateTime
+                }
+            })
+
+            if (!isScheduleExist) {
+                const result = await prisma.schedule.create({
+                    data: interval
+                })
+                schedule.push(result)
+            }
+
             startDateTime.setMinutes(startDateTime.getMinutes() + 30)
         }
-        currentDate.setDate(currentDate.getDay() + 1)
+        currentDate.setDate(currentDate.getDate() + 1)
     }
+    return schedule
 }
 
 export const scheduleServices = {
